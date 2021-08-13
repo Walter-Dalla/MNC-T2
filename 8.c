912fd47ac1c7a0dd66acbf46f1a50a5d945e79d6
Walter Dalla <walter.neto@terceiroslinx.com.br>
2021-08-13 18:52:30 -0300

    faltando trapesio
    Signed-off-by: Walter Dalla <walter.neto@terceiroslinx.com.br>

--- 8.c
+++ 8.c
@@ -9,7 +9,20 @@ double function(double x){
 
 // 3/8 simpson rule function
 
-double UmTercoSimpson(double (*functionAsArg)(), double a, double b,double n) {
+double trapesio(){
+
+    printf("PRECISA IMPLEMENTAR!!!!");
+    return 0;
+}
+
+double UmTercoSimpson(double (*functionAsArg)(), double a, double b,int n) {
+    int oddMode = 0;
+
+    if(n % 2 != 0) {
+        oddMode = 1;
+        n -= 1;
+    }
+
     double h = (b-a)/n;
 
     int i = 0;
@@ -32,6 +45,11 @@ double UmTercoSimpson(double (*functionAsArg)(), double a, double b,double n) {
     double yn = functionAsArg(a + h * n);
 
     double sum = h/3 * (y0 + yn + 4*sumOddIndex + 2*sumEvenIndex) ;
+    
+    if(oddMode == 1) {
+        // o trapesio tem range de h* (n-1) até h*n
+        sum += trapesio();
+    }
 
     return sum;
 
@@ -39,7 +57,7 @@ double UmTercoSimpson(double (*functionAsArg)(), double a, double b,double n) {
 
 int main(void) {
 
-    double result = UmTercoSimpson(function, 1, 4, 2);
+    double result = UmTercoSimpson(function, 1, 4, 3);
 
     printf("%.4lf", result);
     
